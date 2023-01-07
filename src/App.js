36df1617ae5d3e9faf105c61027e89df7a1dc4ea
ryanhkenyon <ryanhkenyon@gmail.com>
2023-01-06 22:27:10 -0500

    nav and landing img

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import {
+  Routes,
+  Route,
+  Link,
+  useParams,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+import { useCookies } from "react-cookie";
 
-function App() {
+import cartB from "./ico/cartB.png";
+import cartW from "./ico/cartW.png";
+import accountB from "./ico/accountB.png";
+import accountW from "./ico/accountW.png";
+import theme from "./ico/theme.png";
+import menuW from "./ico/menuW.png";
+import arkay from "./ico/Arkay.jpg";
+import color from "./img/color.jpg";
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav class="container-fluid">
+        <img src={theme} class="ico" />
+        <img src={accountW} class="ico" />
+        <img src={cartW} class="ico" />
+        <img src={arkay} class="logo" />
+        <img src={menuW} class="menu" />
+      </nav>
+      <div class="landing">
+        <img src={color} class="color" />
+        <input class='search'/>
+        <input class='searchBtn' type='submit'/>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <CookiesProvider>
+      <Router>
+        <App />
+      </Router>
+    </CookiesProvider>
   </React.StrictMode>
 );
 
